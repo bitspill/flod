001bca9b4966d5987a7a41a2707d15e49c8d34e0
Jeremiah Buddenhagen <git@bitspill.net>
2018-10-19 15:17:18 -0700

    partial testnet support

--- chaincfg/params.go
+++ chaincfg/params.go
@@ -409,12 +409,9 @@ var RegressionNetParams = Params{
 var TestNet3Params = Params{
 	Name:        "testnet3",
 	Net:         wire.TestNet3,
-	DefaultPort: "18333",
+	DefaultPort: "17312",
 	DNSSeeds: []DNSSeed{
-		{"testnet-seed.bitcoin.jonasschnelli.ch", true},
-		{"testnet-seed.bitcoin.schildbach.de", false},
-		{"seed.tflo.petertodd.org", true},
-		{"testnet-seed.bluematt.me", false},
+		{"testnet.oip.fun", false},
 	},
 
 	// Chain parameters
@@ -422,40 +419,31 @@ var TestNet3Params = Params{
 	GenesisHash:                  &testNet3GenesisHash,
 	PowLimit:                     testNet3PowLimit,
 	PowLimitBits:                 0x1d00ffff,
-	BIP0034Height:                21111,  // 0000000023b3a96d3484e5abb3755c413e7d41500f8e2a5c3f0dd01299cd8ef8
-	BIP0065Height:                581885, // 00000000007f6655f22f98e72ed80d8b06dc761d5da09df0fa1dc4be4f861eb6
-	BIP0066Height:                330776, // 000000002104c8c45e99a8853285a3b592602a3ccde2b832481da85e9e4ba182
+	BIP0034Height:                33600, // 4ac31d938531317c065405a9b23478c8c99204ff17fc294cb09821e2c2b42e65
+	BIP0065Height:                33600, // 4ac31d938531317c065405a9b23478c8c99204ff17fc294cb09821e2c2b42e65
+	BIP0066Height:                33600, // 4ac31d938531317c065405a9b23478c8c99204ff17fc294cb09821e2c2b42e65
 	CoinbaseMaturity:             100,
-	SubsidyReductionInterval:     210000,
-	TargetTimespan:               time.Hour * 24 * 14, // 14 days
-	TargetTimePerBlock:           time.Minute * 10,    // 10 minutes
-	RetargetAdjustmentFactorUp:   4,                   // 25% less, 400% more
-	RetargetAdjustmentFactorDown: 4,
+	SubsidyReductionInterval:     800000,
+	TargetTimespan:               time.Second * 40 * 6, // 40 seconds * 6 blocks
+	TargetTimePerBlock:           time.Second * 40,     // 40 seconds
+	RetargetAdjustmentFactorUp:   2,
+	RetargetAdjustmentFactorDown: 3,
 	ReduceMinDifficulty:          true,
 	MinDiffReductionTime:         time.Minute * 20, // TargetTimePerBlock * 2
 	GenerateSupported:            false,
 
 	// Checkpoints ordered from oldest to newest.
 	Checkpoints: []Checkpoint{
-		{546, newHashFromStr("000000002a936ca763904c3c35fce2f3556c559c0214345d31b1bcebf76acb70")},
-		{100000, newHashFromStr("00000000009e2958c15ff9290d571bf9459e93b19765c6801ddeccadbb160a1e")},
-		{200000, newHashFromStr("0000000000287bffd321963ef05feab753ebe274e1d78b2fd4e2bfe9ad3aa6f2")},
-		{300001, newHashFromStr("0000000000004829474748f3d1bc8fcf893c88be255e6d7f571c548aff57abf4")},
-		{400002, newHashFromStr("0000000005e2c73b8ecb82ae2dbc2e8274614ebad7172b53528aba7501f5a089")},
-		{500011, newHashFromStr("00000000000929f63977fbac92ff570a9bd9e7715401ee96f2848f7b07750b02")},
-		{600002, newHashFromStr("000000000001f471389afd6ee94dcace5ccc44adc18e8bff402443f034b07240")},
-		{700000, newHashFromStr("000000000000406178b12a4dea3b27e13b3c4fe4510994fd667d7c1e6a3f4dc1")},
-		{800010, newHashFromStr("000000000017ed35296433190b6829db01e657d80631d43f5983fa403bfdb4c1")},
-		{900000, newHashFromStr("0000000000356f8d8924556e765b7a94aaebc6b5c8685dcfa2b1ee8b41acd89b")},
-		{1000007, newHashFromStr("00000000001ccb893d8a1f25b70ad173ce955e5f50124261bbbc50379a612ddf")},
+		{2056, newHashFromStr("d3334db071731beaa651f10624c2fea1a5e8c6f9e50f0e602f86262938374148")},
+		{230000, newHashFromStr("c2e6451240a580c3bfa5ddbfad1b001f8655e7c51d5c32c123e16f69c2d2b539")},
 	},
 
 	// Consensus rule change deployments.
 	//
 	// The miner confirmation window is defined as:
 	//   target proof of work timespan / target proof of work spacing
-	RuleChangeActivationThreshold: 1512, // 75% of MinerConfirmationWindow
-	MinerConfirmationWindow:       2016,
+	RuleChangeActivationThreshold: 600, // 75% of MinerConfirmationWindow
+	MinerConfirmationWindow:       800,
 	Deployments: [DefinedDeployments]ConsensusDeployment{
 		DeploymentTestDummy: {
 			BitNumber:  28,
@@ -464,13 +452,13 @@ var TestNet3Params = Params{
 		},
 		DeploymentCSV: {
 			BitNumber:  0,
-			StartTime:  1456790400, // March 1st, 2016
-			ExpireTime: 1493596800, // May 1st, 2017
+			StartTime:  1483228800, // January 1, 2017
+			ExpireTime: 1530446401, // July 1, 2018
 		},
 		DeploymentSegwit: {
 			BitNumber:  1,
-			StartTime:  1462060800, // May 1, 2016 UTC
-			ExpireTime: 1493596800, // May 1, 2017 UTC.
+			StartTime:  1483228800, // January 1, 2017
+			ExpireTime: 1530446401, // July 1, 2018
 		},
 	},
 
@@ -482,15 +470,16 @@ var TestNet3Params = Params{
 	Bech32HRPSegwit: "tb", // always tb for test net
 
 	// Address encoding magics
-	PubKeyHashAddrID:        0x6f, // starts with m or n
-	ScriptHashAddrID:        0xc4, // starts with 2
+	PubKeyHashAddrID: 115, // starts with m or n
+	ScriptHashAddrID: 198, // starts with 2
+	// 58
 	WitnessPubKeyHashAddrID: 0x03, // starts with QW
 	WitnessScriptHashAddrID: 0x28, // starts with T7n
-	PrivateKeyID:            0xef, // starts with 9 (uncompressed) or c (compressed)
+	PrivateKeyID:            239,  // starts with 9 (uncompressed) or c (compressed)
 
 	// BIP32 hierarchical deterministic extended key magics
-	HDPrivateKeyID: [4]byte{0x04, 0x35, 0x83, 0x94}, // starts with tprv
-	HDPublicKeyID:  [4]byte{0x04, 0x35, 0x87, 0xcf}, // starts with tpub
+	HDPrivateKeyID: [4]byte{0x01, 0x34, 0x40, 0xe2}, // starts with tprv
+	HDPublicKeyID:  [4]byte{0x01, 0x34, 0x3c, 0x23}, // starts with tpub
 
 	// BIP44 coin type used in the hierarchical deterministic path for
 	// address generation.
